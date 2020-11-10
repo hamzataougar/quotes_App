a390ab8831a2120c8cafc89755f76bc6b00b103d
hamzataougar <taougarhamza69@gmail.com>
2020-11-10 23:42:36 +0100

    Fix CORS problem

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-      const response = await fetch(apiUrl);
+      const response = await fetch(proxyUrl + apiUrl);
       const data = await response.text();  
       const json = JSON.parse(data); 
       //Check if Author exists
